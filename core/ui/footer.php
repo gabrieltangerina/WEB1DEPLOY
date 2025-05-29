a02727b01b77a95f266952c2c4db06265d8d694f
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2025-05-29 20:49:59 -0300

    feat: initial commit

--- core/ui/footer.php
+++
@@ -1,7 +0,0 @@
-<div class="container-fluid color-primary text-white d-flex justify-content-center align-items-center mt-5" style="height: 6rem;  width: 100%;">
-    <div class="text-center">
-        <p class="mb-0">Trabalho T1 - VTPDWE2 & VTPISRV</p>
-        <p class="mb-0">Gabriel Tangerina Gonzalez</p>
-        <p class="mb-0">gabriel.tangerina@aluno.ifsp.edu.br</p>
-    </div>
-</div>
--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_destroy();
-
-setcookie("user_logged", "", time() - 3600, "/");
-
-header("Location: index.php");
-exit();
