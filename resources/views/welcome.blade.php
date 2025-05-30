d603ad0db59fc7fa26439fa4dcfe53787d84f190
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2025-05-30 07:31:12 -0300

    feat: alter text welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -883,11 +883,11 @@
                                 </svg>
                             </div>
 
-                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Laracasts</h2>
+                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Fazendo um teste do dia 30/05</h2>
 
                             <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
-                            </p>
+                                COUT << "Vila la vida" << endl;
+                                    </p>
                         </div>
 
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">
