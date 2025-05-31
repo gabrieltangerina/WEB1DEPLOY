57e877e3bb2bb32186d1d1a90f00ff4e1caefa41
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2025-05-31 17:14:25 -0300

    Arrumando rota padrao do sistema para a de produtos e colocando botoes de login e registrar se estiver deslogado

--- sistema-vendas/resources/views/layouts/navigation.blade.php
+++ sistema-vendas/resources/views/layouts/navigation.blade.php
@@ -4,18 +4,18 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
+                <!-- <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
-                </div>
+                </div> -->
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <!-- <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                </div>
+                </div> -->
 
                 <x-nav-link :href="route('produtos.index')" :active="request()->routeIs('produtos.index')">
                     {{ __('Produtos') }}
@@ -27,11 +27,14 @@
             </div>
 
             <!-- Settings Dropdown -->
+            @auth
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                            @auth
                             <div>{{ Auth::user()->name }}</div>
+                            @endauth
 
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -59,6 +62,14 @@
                     </x-slot>
                 </x-dropdown>
             </div>
+            @endauth
+
+            @guest
+            <div class="hidden sm:flex sm:items-center sm:ms-6">
+                <a href="{{ route('login') }}" class="text-sm text-gray-600 hover:text-gray-900 px-4 py-2">Login</a>
+                <a href="{{ route('register') }}" class="ml-2 text-sm text-gray-600 hover:text-gray-900 px-4 py-2">Registrar</a>
+            </div>
+            @endguest
 
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
@@ -83,8 +94,8 @@
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                <div class="font-medium text-base text-gray-800">{{ optional(Auth::user())->name ?? 'Visitante' }}</div>
+                <div class="font-medium text-sm text-gray-500">{{ optional(Auth::user())->email  }}</div>
             </div>
 
             <div class="mt-3 space-y-1">
--- sistema-vendas/routes/web.php
+++ sistema-vendas/routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\VendaController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('produtos.index'); // Ou redirect('/produtos');
 });
 
 Route::get('/dashboard', function () {
